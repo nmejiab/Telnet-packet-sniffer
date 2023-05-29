969f2167626c0473169d1a53efe0ea12d9c16f8f
nmejiab <nmejiab@unal.edu.co>
2023-05-29 13:44:31 -0500

    fix: deprecated funtion lookupdev

--- sniffer/sniffer.c
+++ sniffer/sniffer.c
@@ -3,13 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+    char *dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_if_t *alldevs;
+    pcap_if_t *d;
 
-	dev = pcap_lookupdev(errbuf);
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
-	}
-	printf("Device: %s\n", dev);
-	return(0);
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        fprintf(stderr, "Error finding devices: %s\n", errbuf);
+        return 2;
+    }
+
+    dev = alldevs->name;
+    printf("Device: %s\n", dev);
+
+    pcap_freealldevs(alldevs);
+    return 0;
 }
