2b3b0f3ecca5c956fc795da0734cce3b92d1379f
nmejiab <nmejiab@unal.edu.co>
2023-05-29 15:06:48 -0500

    add: pcap install rutine for Cent Os

---
+++ installPcapCOS.sh
@@ -0,0 +1,5 @@
+sudo yum update
+sudo yum groupinstall "Development Tools"
+sudo yum install libpcap-devel
+gcc --version
+pcap-config --version
--- installPcapDeb.sh
+++ installPcapDeb.sh
@@ -1,5 +1,2 @@
 sudo apt-get update
 sudo apt-get install libpcap-dev
-sudo yum install libpcap-devel
-gcc --version
-pcap-config --version
